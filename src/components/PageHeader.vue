df73c055226ffcdea3bd661a9a7ec75fd7034ad1
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-15 20:38:17 +0200

    aggiunta link e foto jumbotron

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,6 +4,55 @@
 
 export default {
     name: "PageHeader",
+    data() {
+      return {
+        links: [
+          {
+            text: "HOME",
+            url: "#",
+            current: true,
+
+          },
+          {
+            text: "ABOUT",
+            url: "#",
+            current: false,
+
+          },
+          {
+            text: "COURSES NEW",
+            url: "#",
+            current: false,
+
+          },
+          {
+            text: "EVENTS",
+            url: "#",
+            current: false,
+
+          },
+          {
+            text: "FACILITIES",
+            url: "#",
+            current: false,
+
+          },
+          {
+            text: "NEWS",
+            url: "#",
+            current: false,
+
+          },
+          {
+            text: "ADMISSION APPLY",
+            url: "#",
+            current: false,
+
+          },
+          
+        ]
+      }
+    }
 
 }
 </script>
@@ -28,6 +77,27 @@ export default {
         </ul>
       </div>
     </div>
+    <div class="container_logo">
+      <div>
+        <img src="../assets/img/university_logo.png" alt="">
+      </div>
+      <div>
+       <nav>
+        <ul>
+          <li v-for="link in links">
+            <a :href="link.url">
+              {{ link.text }}
+            </a>
+          </li>
+        </ul>
+       </nav>
+      </div>
+    </div>
+    <div class="container_image">
+      <img src="../assets/img/home_main_slider_bg.jpg" alt="">
+        
+
+      </div>
   </header>
 
 
@@ -58,7 +128,26 @@ ul{
 }
 li{
   padding: 20px;
+  margin: 0 auto;
 }
+.container_logo{
+  width: 100%;
+  height: 100px;
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+a{
+  text-decoration: none;
+  color: black;
+  font-size: 10px;
+}
+.container_image{
+  width: 100%;
+  height: 350px;
+}
+
  
 
 </style>
