6403a02e1bdc3c2bb0a04d4b304bccb09e45334a
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-15 10:22:38 +0200

    creazione mixins e aggiornamento pageheader

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,19 @@
+<script>
+
+
+export default {
+    name: "PageHeader",
+
+}
+</script>
+
+<template>
+  
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+ 
+
+</style>
