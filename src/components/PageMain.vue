4b3a428e84bedf3c46c7cf235ffe2c1b5bff5e56
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-16 18:52:29 +0200

    continuo woking su main

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -15,8 +15,8 @@ export default {
             <p>We offer a wide range of high quality of teaching an extra-curricular activies</p>
         </section>
         <section>
-            <div id="why">
-                <div id="offer">
+            <div class="why">
+                <div class="offer">
                     <h2>WHY CHOOSE AVADA UNIVERSITY?</h2>
                     <p>We offer a range of training packages in a range of subject areas and can offer blended learning opportunities o best meet your needs. If sitting in a room with a trainer and having the opportunity for discussion with other learners sounds good you then we can offer this. If you prefer to study in your own time and at your own speed then one of our e-learning packages may be right up your street.Ther is a third option,which is you like the sound of both but perhaps can't attend all training sessions,thi is when blended learning come into its own.  </p>
                 </div>
@@ -24,6 +24,43 @@ export default {
                     <img src="../assets/img/fss-demo-main-400x300.jpg" alt="">
                 </div>
             </div>
+        </section>
+        <section>
+            <div class="why">
+                <div class="offer">
+                  <h2>WORLD CLASS FACILITIES</h2>
+                  <p>Avada Univesrity is devoted to excellence in teaching, learning and research, and developing leaders in many disciplines who make a difference globally. </p>
+                  <p><strong>International focus</strong>- our students come from acrossthe globe</p>
+                  <p><strong>Residential courses</strong>- we encourage learning and growth</p>
+                  <p><strong>People no profits</strong>- education is right not a privilege</p>
+                  <p>if you prefer to study own and your than speed our learning packages</p>
+                </div>
+                <div>
+                  <h2>OUR VALUES</h2>
+                  <ul id="orange">
+                    <li>+ OUR MISSION</li>
+                    <li>+ OUR PHILOSOPHY</li>
+                    <li>+ OUR COMMITMENT</li>
+                    <li>+ OUR PROMISE</li>
+                    <li>+ OUR VALUES</li>
+                  </ul>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div id="container-viewmore">
+                <div id="view-1">
+                     
+
+                </div>
+                <div id="view-2">
+
+                </div>
+                <div id="view-3">
+
+                </div>
+            </div>
+       
         </section>
     </main>
 
@@ -37,7 +74,7 @@ export default {
     text-align: center;
     margin-top: 50px;
 }
-#why{
+.why{
     display: flex;
     flex-direction: row;
     justify-content: space-around;
@@ -46,9 +83,27 @@ export default {
     line-height: 30px;
     height: 400px;
 }
-#offer{
+.offer{
     width: 50%;
 }
+#container-viewmore{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    height: 400px;
+    width: 100%;
+    background-color: yellow;
+}
+#view-1{
+    border-color: black;
+}
+#view-2{
+    border-color: black;
+}
+#view-3{
+    border-color: black;
+}
+
  
 
 </style>
